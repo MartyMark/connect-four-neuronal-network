ceb1b7d8678a63acb79e63fcda53e0b1347feccf
freddytran <38252781+freddytran@users.noreply.github.com>
2022-08-12 21:12:34 +0200

    player.py - added DocStrings and changed Variable names to snake_case naming style

--- src/player.py
+++ src/player.py
@@ -1,7 +1,10 @@
+"""
+This Module holds the Player Class.
+"""
 import math
 import random
-import numpy as np
 import copy
+import numpy as np
 from min_max import MinMaxAlgorithm
 
 from game import RED_PLAYER_VAL
@@ -9,7 +12,7 @@ from operation_util import get_available_moves
 
 
 class Player:
-
+    """This Class holds all neccessary Methods for a Player."""
     def __init__(self, value, strategy='random', model=None):
         self.value = value
         self.strategy = strategy
@@ -18,6 +21,7 @@ class Player:
 
     # [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]]
     def get_move(self, board):
+        """Decides the next move, depending on the strategy and returns the move."""
         # [[5,0], [5,1], [5,2], [5,3], [5,4], [5,5], [5,6]]
         available_moves = get_available_moves(board)
 
@@ -43,17 +47,18 @@ class Player:
         else:
             max_value = 0
             best_move = available_moves[0]
-            for availableMove in available_moves:
+            for available_move in available_moves:
                 board_copy = copy.deepcopy(board)
-                board_copy[availableMove[0]][availableMove[1]] = self.value
+                board_copy[available_move[0]][available_move[1]] = self.value
                 if self.value == RED_PLAYER_VAL:
                     value = self.model.predict(board_copy, 2)
                 else:
                     value = self.model.predict(board_copy, 0)
                 if value > max_value:
                     max_value = value
-                    best_move = availableMove
+                    best_move = available_move
             return best_move
 
     def get_player(self):
+        """Returns the current player."""
         return self.value

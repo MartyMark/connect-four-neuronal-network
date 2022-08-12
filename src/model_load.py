1f8a56adc79ac41e773295da711aff3a53eef64c
freddytran <38252781+freddytran@users.noreply.github.com>
2022-08-12 20:31:41 +0200

    model_load.py - added Docstring and change Variable names to snake_style naming style

--- src/model_load.py
+++ src/model_load.py
@@ -1,12 +1,17 @@
+"""
+This Modules holds the ConnectFourModelLoad Class.
+"""
 import numpy as np
 from tensorflow import keras
 
 
 class ConnectFourModelLoad:
-
+    """This Class loads the Model from the nn_Model Directory."""
     def __init__(self, number_of_inputs):
-        self.numberOfInputs = number_of_inputs
+        """Init function of the Class."""
+        self.number_of_inputs = number_of_inputs
         self.model = keras.models.load_model('nn_model')
 
     def predict(self, data, index):
-        return self.model.predict(np.array(data).reshape(-1, self.numberOfInputs))[0][index]
+        """Calls the Predict method of the Model."""
+        return self.model.predict(np.array(data).reshape(-1, self.number_of_inputs))[0][index]
